cb3acd0364f8370cad2841723854c9b38cd2aa85
Ashutosh8454 <ashutosh703325@gmail.com>
2022-02-01 23:58:19 +0530

    nav modifi and hero section add

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,19 @@
-console.log("Hello world");
+function Drop_down() {
+    let Drop_down_list = document.getElementById('drop');
+    if (Drop_down_list.style.display == "none") {
+
+        Drop_down_list.style.display = 'block';
+    } else {
+        Drop_down_list.style.display = 'none';
+    }
+}
+let i = 0;
+
+function hide() {
+    i++;
+    if (i == 2) {
+        let Drop_down_list = document.getElementById('drop');
+        Drop_down_list.style.display = 'none';
+        i = 0;
+    }
+}
